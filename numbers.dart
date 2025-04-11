c70a91b4505257ec391041740fafb33c1c36ec73
Antonio Maroca A. Pinto Coelho <antonioalemaroca@gmail.com>
2025-04-11 14:13:15 -0300

    Create numbers.dart

---
+++ numbers.dart
@@ -0,0 +1,24 @@
+void main (){
+  
+  int item1 = 300;
+  int item2 = 100;
+  double price = 2.99;
+  
+    print(
+      5 + 3 * 4 - (4 / 2)
+    ); 
+  
+    item1 += 25;
+  // adiciona um valor a variavel atual de forma simples
+  
+    print(
+      item1
+    );
+  
+    item2++;
+  // adiciona 1 para a variavel contavel
+  
+    print(
+      item2
+    );
+}
