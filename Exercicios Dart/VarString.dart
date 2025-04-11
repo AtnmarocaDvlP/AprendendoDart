118e6f605aa5b4a10229710535561e567451df14
Antonio Maroca A. Pinto Coelho <antonioalemaroca@gmail.com>
2025-04-11 13:33:16 -0300

    Update VarString.dart

--- Exercicios Dart/VarString.dart
+++ Exercicios Dart/VarString.dart
@@ -10,15 +10,27 @@ void main(){
   
   print (
     Nameone.toLowerCase()
+    // escreve a String em minusculo
   );
   print (
     Nameone.toUpperCase()
+    // escreve a String em Maiusculo
   );
   print (
     Nameone.length
+    // mostra a quantidade de caracteres em determinada String
   );
   print (
     Nameone[3]
+    // mostra o caracter relacionado a posicao do index 
+  );
+  print (
+    Nameone.indexOf("e")
+    // Corresponde em posicao do index em relacao ao caracter digitado
+  );
+  print (
+    Nameone.contains("d")
+    // escreve se a String Possui o Caracter escrito (true/false)
   );
   
   
