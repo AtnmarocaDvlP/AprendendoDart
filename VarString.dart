5ef4b63abd8eafddb6127433eef7e130feef35af
Antonio Maroca A. Pinto Coelho <antonioalemaroca@gmail.com>
2025-04-11 13:24:58 -0300

    Create VarString.dart
    algumas variaveis para a String

---
+++ VarString.dart
@@ -0,0 +1,26 @@
+void main(){
+  
+  String Nameone = "Antonio";
+  
+  print ("${Nameone} foi passear!");
+    
+    Nameone = "Pedro";
+  
+  // funcoes para a String
+  
+  print (
+    Nameone.toLowerCase()
+  );
+  print (
+    Nameone.toUpperCase()
+  );
+  print (
+    Nameone.length
+  );
+  print (
+    Nameone[3]
+  );
+  
+  
+}
+  
