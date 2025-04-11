44dee451d2caa4566a8667257483a829dbeb4289
Antonio Maroca A. Pinto Coelho <antonioalemaroca@gmail.com>
2025-04-11 13:23:52 -0300

    Create basico.dart

---
+++ basico.dart
@@ -0,0 +1,12 @@
+void main(){
+  String name1 = "Antonio";
+  String Age = "19";
+  
+  print("O nome dele era ${name1}");
+  print("${name1} realmente gostava de seu nome");
+  
+    name1 = "Lucas";  
+  
+  print("${name1} Tinha ${Age} anos");
+  print("testando apenas uma vez mais ${name1}");
+}
