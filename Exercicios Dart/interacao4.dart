b409a187c65ee92b33aecbc816b6a7dc5e2e0b40
Antonio Maroca A. Pinto Coelho <antonioalemaroca@gmail.com>
2025-04-11 14:56:48 -0300

    Update interacao4.dart

---
+++ Exercicios Dart/interacao4.dart
@@ -0,0 +1,12 @@
+import "dart:math";
+import "dart:io";
+
+// Aprendendo a fazer a maquina interagir com voce
+  void main(){
+    
+    print("Qual e o seu nome?: ");
+    String username = stdin.readLineSync() ?? "Desconhecido";
+  print("Olá, ${username}!");
+    
+    
+  }
