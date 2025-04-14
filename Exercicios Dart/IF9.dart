feca3b350d61704bd5da4c60b5e6e9eaf005cfd3
Antonio Maroca A. Pinto Coelho <antonioalemaroca@gmail.com>
2025-04-14 14:19:26 -0300

    Update IF9.dart

--- Exercicios Dart/IF9.dart
+++ Exercicios Dart/IF9.dart
@@ -1,15 +1,34 @@
+// import "dart:math";
+// import "dart:io";
+
+// void main(){
+    
+//     bool honored = false;
+    
+//     if(honored){
+//         print("voce e o mais honrado. ");
+//     } else {
+//         print("voce nao e especial. ");
+//     }
+    
+// }
+// // o basico
 import "dart:math";
 import "dart:io";
 
 void main(){
     
-    bool honored = false;
+    print("escolha entre 1 ou 2: ");
+    int num1 = int.parse(stdin.readLineSync()!);
     
-    if(honored){
+    if(num1 == 1){
         print("voce e o mais honrado. ");
-    } else {
+    } else if(num1 == 2) {
         print("voce nao e especial. ");
+    } else {
+        print("esta opcao nao existe!");
     }
     
 }
-// o basico
+
+// um pouco mais complexo
