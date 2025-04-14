2e3da0534842c686a9907f65054634d4bd120403
Antonio Maroca A. Pinto Coelho <antonioalemaroca@gmail.com>
2025-04-14 14:46:27 -0300

    Update calculator5.dart

--- Exercicios Dart/calculator5.dart
+++ Exercicios Dart/calculator5.dart
@@ -2,19 +2,38 @@ import"dart:math";
 import"dart:io";
 
 void main(){
-    print("Digite O primeiro Valor: ");
-    double num1 = double.parse(stdin.readLineSync()!);
-    
-    print("Digite o segundo valor: ");
-    double num2 = double.parse(stdin.readLineSync()!);
     
     print("escolha a operacao que quer fazer: ");
     print(" digite 1 para soma.");
     print(" digite 2 para subtração.");
     print(" digite 3 para multiplicação.");
     print(" digite 4 para divisão.");
+    print(" digite 5 para porcentagem(primeiro valor depois a porcentagem!)");
+    print(" digite 6 para raiz quadrada(somente o primeiro numero!)");
     
-    String op = stdin.readLineSync()!;
+    int op = int.parse(stdin.readLineSync()!);
+    
+    print("Digite O primeiro Valor: ");
+    double num1 = double.parse(stdin.readLineSync()!);
+    
+    print("Digite o segundo valor: ");
+    double num2 = double.parse(stdin.readLineSync()!);
+    
+    if(op == 1){
+        print(num1 + num2);
+    } else if(op == 2){
+        print(num1 - num2);
+    } else if(op == 3){
+        print(num1 * num2);
+    }else if(op == 4){
+        print(num1 / num2);
+    }else if(op == 5){
+        print(num1 * num2 / 100);
+    }else if(op == 6){
+        print(sqrt(num1));
+    } else {
+        print("operacao nao reconhecida!");
+    }
 }
 
-//usar double.parse para poder usar a string como double ou int.parse para o mesmo.
+// calculadora 100% funcional
