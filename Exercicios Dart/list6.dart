7e771686a4c0b2d9e5704ae4f867feab92a1f551
Antonio Maroca A. Pinto Coelho <antonioalemaroca@gmail.com>
2025-04-14 11:47:08 -0300

    Update list6.dart

--- Exercicios Dart/list6.dart
+++ Exercicios Dart/list6.dart
@@ -6,6 +6,7 @@ void main(){
     List <int> RandNums = [2 ,4 ,5 ,7 ,72];
     //   index positions   0. 1. 2. 3. 4.  
     
+    // adicionar numeros a lista: RandNums.add(32);
     RandNums[3] = 12;
     
     print(
