4a82787f01414bb57fbbcddce47c5b24dd504922
Antonio Maroca A. Pinto Coelho <antonioalemaroca@gmail.com>
2025-04-14 11:45:20 -0300

    Create list6.dart

---
+++ Exercicios Dart/list6.dart
@@ -0,0 +1,16 @@
+import "dart:math";
+import "dart:io";
+
+void main(){
+    
+    List <int> RandNums = [2 ,4 ,5 ,7 ,72];
+    //   index positions   0. 1. 2. 3. 4.  
+    
+    RandNums[3] = 12;
+    
+    print(
+        RandNums[3] + RandNums[2]
+    );
+    
+    
+}
